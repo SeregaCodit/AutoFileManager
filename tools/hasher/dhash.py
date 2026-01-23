50d8b0840316bdf1972e2ddc0c991a8cd4c6d6e2
SeregaCodit <serega.codit@gmail.com>
2026-01-23 23:13:55 +0200

    refactor(dhash): remove __init__

--- tools/hasher/dhash.py
+++ tools/hasher/dhash.py
@@ -8,13 +8,6 @@ from tools.hasher.base_hasher import BaseHasher
 
 class DHash(BaseHasher):
     """dHash compare algorithm"""
-    def __init__(
-            self,
-            image_dir: Path,
-            hash_size: Union[Tuple[int, int], int] = 16
-    ):
-        super().__init__(image_dir)
-        self.hash_size = hash_size
 
     def compute_hash(self, image_path: Path) -> Union[np.ndarray, None]:
         image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -29,17 +22,5 @@ class DHash(BaseHasher):
         return gradient_difference.flatten()
 
 
-    @property
-    def hash_size(self) -> int:
-        return self._hash_size
 
-    @hash_size.setter
-    def hash_size(self, value: Union[Tuple[int, int], int]) -> None:
-        if isinstance(value, int):
-            self._hash_size = value
-        elif isinstance(value, tuple):
-            self._hash_size = value[0] if value[0] <= 0 else value[1]
-        else:
-            self.logger.error(f"hash size must be int or tuple, got {type(value)}")
-            raise TypeError(f"hash size must be int or tuple, got {type(value)}")
 
