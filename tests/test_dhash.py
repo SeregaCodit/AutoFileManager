2b2e630961383c8d2ca89c6a8efb059eac1e1ddc
Serhii Naumenko <naumenko.s.mail@gmail.com>
2026-01-24 16:06:25 +0200

    feat(tests):  refactor tests, add test_hash_size

--- tests/test_dhash.py
+++ tests/test_dhash.py
@@ -1,25 +1,54 @@
+import numpy as np
 import pytest
 
 from tools.comparer.img_comparer.hasher.dhash import DHash
 
-
-def test_threshold():
+@pytest.mark.parametrize("input_value, expected_vals", [
+    (0.1, 0),    # int(0.1) * (16*16) = 0
+    (10, 25),    # int((10 / 100 ) * (16*16)) = 25
+    (1, 2),       # int(1 / 100) * (16*16) = 2
+])
+def test_threshold(input_value, expected_vals):
     """test threshold setter"""
 
     hasher = DHash(
         hash_type="dhash",
-        threshold=10,
-        hash_size=16
+        hash_size=16,
+        threshold=input_value
     )
+    assert isinstance(hasher.threshold, int)
+    assert hasher.threshold == expected_vals
 
-    for i in [10, 10.0, "10"]:
-        hasher.threshold = i
-        assert isinstance(hasher.threshold, int)
+@pytest.mark.parametrize("input_value, expected_val", [
+    (16, 16),
+    ((10, 25), 10),
+    (17.5, 17),
+    ("8", 8)
+])
+def test_hash_size(input_value, expected_val):
 
-def test_hash_size():
+    hasher = DHash(
+        hash_type="dhash",
+        hash_size=input_value,
+        threshold=10
+    )
+@pytest.mark.parametrize("input_value, expected_val", [
+    # equal hashes
+    ((np.array([0, 1, 0, 1]), np.array([0, 1, 0, 1])), 0),
+    # dist == 1
+    ((np.array([0, 0, 0, 1]), np.array([0, 1, 0, 1])), 1),
+    # dist == 2
+    ((np.array([0, 0, 0, 0]), np.array([0, 1, 0, 1])), 2),
+    # dist == 3
+    ((np.array([1, 0, 1, 1]), np.array([0, 0, 0, 0])), 3),
+    # dist == 4
+    ((np.array([0, 0, 0, 0]), np.array([1, 1, 1, 1])), 4)
+])
+def test_calculate_distance(input_value, expected_val):
     hasher = DHash()
+    hash1, hash2 = input_value
+    dist = hasher.calculate_distance(hash1, hash2)
 
-    for i in [16, 16.0, "16", (16, 16)]:
-        hasher.hash_size = i
-        assert isinstance(hasher.hash_size, int)
+    assert isinstance(dist, int)
+    assert dist == expected_val
 
