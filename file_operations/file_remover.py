9a0bd966e459dd09c5d8eda70546faf0c1836225
Serhii Naumenko <naumenko.s.mail@gmail.com>
2026-01-25 09:29:04 +0200

    refactor(delete): add type hinting and TypeError rising if unsupportable type received

--- file_operations/file_remover.py
+++ file_operations/file_remover.py
@@ -1,19 +1,22 @@
-from os import remove
 from pathlib import Path
-from typing import Union, List
+from typing import Union, List, Tuple
 
 from logger.logger_protocol import LoggerProtocol
 
+
 class FileRemoverMixin:
     """internal helper for file removing functionality"""
-    def _remove_all(self, filepaths: Union[List[Path], Path]) -> None:
+    def _remove_all(self, filepaths: Union[List[Path], Tuple[Path], Path]) -> None:
         """deletes all received files"""
-        if isinstance(filepaths, list):
+        if isinstance(filepaths, (list, tuple)):
             for path in filepaths:
                 self._remove_file(path)
 
-        if isinstance(filepaths, Path):
+        elif isinstance(filepaths, Path):
             self._remove_file(filepaths)
+
+        else:
+            raise TypeError(f'filepaths should be a list or a tuple or a Path, not {type(filepaths)}')
 
 
     def _remove_file(self: LoggerProtocol, path: Path) -> None:
